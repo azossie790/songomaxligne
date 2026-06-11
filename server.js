9371bb0d0899b810fc5ca0938a4dfbb8fa4be9e7
azossie790 <azossiemaxim633@gmail.com>
2026-06-11 12:56:15 +0200

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const path = require('path');
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'publique')));
 
 // ─── ÉTAT DU JEU ──────────────────────────────────────────────────────────────
 // Plateau : cases[0..6] = SUD (joueur SUD), cases[7..13] = NORD (joueur NORD)
